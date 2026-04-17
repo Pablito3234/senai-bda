e2a859d2331e77bee1d11fc00cdba1abe8268772
Pablo Gascon <pablogasconiel445@gmail.com>
2026-04-17 01:02:32 -0300

    added session factory configuration

--- src/main/java/config/HibernateUtil.java
+++ src/main/java/config/HibernateUtil.java
@@ -1,4 +1,21 @@
 package config;
 
+import org.hibernate.HibernateError;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 public class HibernateUtil {
+    private static SessionFactory sessionFactory;
+
+    public static SessionFactory getSessionFactory() throws HibernateError{
+        if (sessionFactory == null){
+            buildSessionFactory();
+        }
+        return sessionFactory;
+    }
+
+    private static void buildSessionFactory() throws HibernateError {
+        Configuration configuration = new Configuration();
+        sessionFactory = configuration.configure().buildSessionFactory();
+    }
 }
